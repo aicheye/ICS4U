dcae251853984201b9bb2982d39109fbc62440ca
Sean <contact@seanyang.me>
2025-02-20 23:12:50 -0500

    Add initial implementation of AddDigits program with scanner closure and input processing

--- homework/HW_04_Gr11Review/AddDigits.java
+++ homework/HW_04_Gr11Review/AddDigits.java
@@ -1,3 +1,40 @@
-public class AddDigits {
+import java.util.Scanner;
 
+/**
+ * Programmer: Sean Yang
+ * Program Name: AddDigits
+ * Date: 20/02/2025
+ * Description:
+ */
+
+public class AddDigits {
+    public static void main(String[] args) {
+        // declare scanner and variables
+        Scanner sc = new Scanner(System.in);
+        String input;
+        int ans;
+
+        // take input
+        input = sc.nextLine();
+
+        // calculate answer
+        ans = 0;
+        for (int i = 0; i < input.length(); i++) {
+            ans += input.charAt(i) - '0';
+        }
+        System.out.println(ans);
+
+        // output each character
+        System.out.print("(");
+        System.out.print(input.charAt(0));
+
+        for (int i = 1; i < input.length(); i++) {
+            System.out.print("+" + input.charAt(i));
+        }
+
+        System.out.println(")");
+
+        // close scanner
+        sc.close();
+    }
 }
